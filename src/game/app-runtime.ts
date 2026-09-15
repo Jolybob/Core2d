c9408267514099b412b5b11ae63df10983f27ea1
Jolybob <ohbelisque@gmail.com>
2026-09-15 22:45:19 +0200

    feat: compose authoritative combat runtime

--- src/game/app-runtime.ts
+++ src/game/app-runtime.ts
@@ -43,7 +43,8 @@ export class GameRuntime {
     this.economy = new EconomySystem(this.store);
     this.fishing = new FishingSystem(this.store, this.events);
     this.player = new PlayerSystem(this.store, this.worldRuntime);
-    this.combat = new CombatSystem(this.store, this.player);
+    this.combat = new CombatSystem(this.store, this.player, this.worldRuntime, this.events);
+    this.combat.refresh();
     this.day = new DaySystem(this.store, this.farming, this.worldRuntime, this.player);
     this.resources = new ResourceSystem(this.store, this.events, this.worldRuntime);
     this.commandHandler = new GameCommandHandler({ store: this.store, worldRuntime: this.worldRuntime, combat: this.combat, crafting: this.crafting, day: this.day, economy: this.economy, farming: this.farming, fishing: this.fishing, player: this.player, resources: this.resources });
