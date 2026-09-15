f9afc27cea3188e276b1d12e8cf8b19638e0bb27
Jolybob <ohbelisque@gmail.com>
2026-09-15 22:45:53 +0200

    feat: align legacy runtime with combat loop

--- src/game/runtime.ts
+++ src/game/runtime.ts
@@ -38,21 +38,15 @@ export class GameRuntime {
     this.quests = new QuestSystem(this.store, this.events);
     const initialState = this.store.getState();
     this.worldRuntime = new WorldRuntime(createInitialWorldSave(2042));
-    const center = worldToChunk({
-      x: Math.floor(initialState.player.x / TILE_SIZE),
-      y: Math.floor(initialState.player.y / TILE_SIZE),
-    });
-    for (let y = center.y - INITIAL_CHUNK_RADIUS; y <= center.y + INITIAL_CHUNK_RADIUS; y += 1) {
-      for (let x = center.x - INITIAL_CHUNK_RADIUS; x <= center.x + INITIAL_CHUNK_RADIUS; x += 1) {
-        this.worldRuntime.loadChunk({ x, y });
-      }
-    }
+    const center = worldToChunk({ x: Math.floor(initialState.player.x / TILE_SIZE), y: Math.floor(initialState.player.y / TILE_SIZE) });
+    for (let y = center.y - INITIAL_CHUNK_RADIUS; y <= center.y + INITIAL_CHUNK_RADIUS; y += 1) for (let x = center.x - INITIAL_CHUNK_RADIUS; x <= center.x + INITIAL_CHUNK_RADIUS; x += 1) this.worldRuntime.loadChunk({ x, y });
     this.farming = new FarmingSystem(this.store, this.events, this.worldRuntime);
     this.crafting = new CraftingSystem(this.store);
     this.economy = new EconomySystem(this.store);
     this.fishing = new FishingSystem(this.store, this.events);
     this.player = new PlayerSystem(this.store, this.worldRuntime);
-    this.combat = new CombatSystem(this.store, this.player);
+    this.combat = new CombatSystem(this.store, this.player, this.worldRuntime, this.events);
+    this.combat.refresh();
     this.day = new DaySystem(this.store, this.farming, this.worldRuntime, this.player);
     this.resources = new ResourceSystem(this.store, this.events, this.worldRuntime);
     this.commandHandler = new GameCommandHandler({ store: this.store, worldRuntime: this.worldRuntime, combat: this.combat, crafting: this.crafting, day: this.day, economy: this.economy, farming: this.farming, fishing: this.fishing, player: this.player, resources: this.resources });
