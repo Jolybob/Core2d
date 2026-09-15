b112801f61d1077a1f841d1b22ed1fc0180eb34b
Jolybob <ohbelisque@gmail.com>
2026-09-15 03:04:35 +0200

    feat: connect backpack item quickbar

--- src/entry.js
+++ src/entry.js
@@ -2,5 +2,7 @@ import './ui-fix.ts';
 import './polish.css';
 import './corekeeper-ui.css';
 import './game-ui.css';
+import './backpack-quickbar.css';
 import './version.ts';
+import './backpack-quickbar.ts';
 import './cozy-farm.ts';
