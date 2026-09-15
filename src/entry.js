6bf6699ea6c61de5bce909654eff7c71478710f7
Jolybob <ohbelisque@gmail.com>
2026-09-15 02:33:39 +0200

    feat: make quick bar the primary gameplay control

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,7 @@
 import './ui-fix.ts';
 import './polish.css';
 import './corekeeper-ui.css';
+import './quickbar-fix.css';
 import './version.ts';
+import './quickbar-fix.ts';
 import './cozy-farm.ts';
