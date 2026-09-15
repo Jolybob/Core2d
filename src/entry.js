9580048944efe940b14b0e04cacc90409c9a12af
Jolybob <ohbelisque@gmail.com>
2026-09-15 03:35:33 +0200

    refactor: remove duplicate untyped entry point

--- src/entry.js
+++
@@ -1,9 +0,0 @@
-import './ui-fix.ts';
-import './polish.css';
-import './corekeeper-ui.css';
-import './game-ui.css';
-import './backpack-quickbar.css';
-import './accessibility-fix.ts';
-import './version.ts';
-import './backpack-quickbar.ts';
-import './cozy-farm.ts';
