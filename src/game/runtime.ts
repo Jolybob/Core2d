c3de66652e180896f9fbf02ad5618471889b175c
Jolybob <ohbelisque@gmail.com>
2026-09-15 23:09:07 +0200

    feat: wire player survival events into runtime

--- src/game/runtime.ts
+++ src/game/runtime.ts
@@ -40,7 +40,7 @@ export class GameRuntime {
     this.crafting = new CraftingSystem(this.store);
     this.economy = new EconomySystem(this.store);
     this.fishing = new FishingSystem(this.store, this.events);
-    this.player = new PlayerSystem(this.store, this.worldRuntime);
+    this.player = new PlayerSystem(this.store, this.worldRuntime, this.events);
     this.combat = new CombatSystem(this.store, this.player, this.worldRuntime, this.events);
     this.day = new DaySystem(this.store, this.farming, this.worldRuntime, this.player);
     this.resources = new ResourceSystem(this.store, this.events, this.worldRuntime);
