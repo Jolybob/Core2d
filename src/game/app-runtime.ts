81476cbdd084244ec8ce0d482df660fd019838f7
Jolybob <ohbelisque@gmail.com>
2026-09-15 22:36:38 +0200

    fix: inject world runtime into app day system

--- src/game/app-runtime.ts
+++ src/game/app-runtime.ts
@@ -44,7 +44,7 @@ export class GameRuntime {
     this.fishing = new FishingSystem(this.store, this.events);
     this.player = new PlayerSystem(this.store, this.worldRuntime);
     this.combat = new CombatSystem(this.store, this.player);
-    this.day = new DaySystem(this.store, this.farming, this.player);
+    this.day = new DaySystem(this.store, this.farming, this.worldRuntime, this.player);
     this.resources = new ResourceSystem(this.store, this.events, this.worldRuntime);
     this.commandHandler = new GameCommandHandler({ store: this.store, worldRuntime: this.worldRuntime, combat: this.combat, crafting: this.crafting, day: this.day, economy: this.economy, farming: this.farming, fishing: this.fishing, player: this.player, resources: this.resources });
   }
